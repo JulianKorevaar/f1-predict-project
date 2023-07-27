03e5c67146737ba2ff209cd93b5ba06da35ff49d
Julian Korevaar <julian.korevaar@deptagency.com>
2023-07-27 19:07:03 +0200

    change deadline

--- src/pages/predict/[race_id].tsx
+++ src/pages/predict/[race_id].tsx
@@ -152,7 +152,7 @@ const Predict: NextPage<PageProps> = (props) => {
       .subtract(2, 'days')
       .local() // convert to local time zone
       .toDate();
-    deadline.setHours(23, 59, 59, 59);
+    deadline.setHours(16, 59, 59, 59);
 
     if (currentName === null || currentName === '') {
       alert('Je bent niet (goed) ingelogd, log alsjeblieft opnieuw in!');
