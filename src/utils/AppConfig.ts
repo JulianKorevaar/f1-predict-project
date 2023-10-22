e223d63a2b25bfbae05cf3543217e3c72e1bd4bf
Julian Korevaar <julian.korevaar@deptagency.com>
2023-10-22 23:26:15 +0200

    update for GP usa

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -6,6 +6,6 @@ export const AppConfig = {
   points_good_pick_kwali: [3, 2, 1],
   points_good_pick_race: [3, 2, 1],
   points_good_pick_bonus: 3,
-  amount_of_races: 22,
+  amount_of_races: 23,
   current_race: 19,
 };
