8730ed7d2049334903acdead367a99faa1084bbc
Julian Korevaar <julian.korevaar@deptagency.com>
2023-11-16 16:02:59 +0100

    update for GP USA

--- src/pages/predict/[race_id].tsx
+++ src/pages/predict/[race_id].tsx
@@ -155,10 +155,10 @@ const Predict: NextPage<PageProps> = (props) => {
 
   const handlePredictButtonClick = async () => {
     const deadline = moment(race?.date)
-      .subtract(2, 'days')
+      .subtract(1, 'days')
       .local() // convert to local time zone
       .toDate();
-    deadline.setHours(18, 59, 59, 59);
+    deadline.setHours(8, 59, 59, 59);
 
     if (currentName === null || currentName === '') {
       alert('Je bent niet (goed) ingelogd, log alsjeblieft opnieuw in!');
