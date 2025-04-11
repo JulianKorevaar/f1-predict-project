f2e06a5ac51d41514522f76a9d8b36671861dae0
juliankorevaar <korevaar.julian2000@gmail.com>
2025-04-11 13:34:29 +0200

    feat: change race to 4

--- src/components/predict/PredictRaceInfo.tsx
+++ src/components/predict/PredictRaceInfo.tsx
@@ -15,7 +15,7 @@ type IHeroRaceInfoProps = {
 const PredictRaceInfo = (props: IHeroRaceInfoProps) => {
   // Create a new Date object for the deadline (2 days before the race date)
   const deadline = moment(props.date).subtract(1, 'days').toDate();
-  deadline.setHours(7, 59, 59, 59);
+  deadline.setHours(17, 59, 59, 59);
 
   return (
     <header className="text-center">
