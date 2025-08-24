0ac891971787cef561d5093b8c93b27fe91fee51
juliankorevaar <korevaar.julian2000@gmail.com>
2025-08-24 13:42:10 +0200

    feat: change race to 15

--- src/pages/predict/[race_id].tsx
+++ src/pages/predict/[race_id].tsx
@@ -168,7 +168,7 @@ const Predict: NextPage<PageProps> = (props) => {
       .subtract(1, 'days')
       .local() // convert to local time zone
       .toDate();
-    deadline.setHours(15, 59, 59, 59);
+    deadline.setHours(14, 59, 59, 59);
 
     if (currentName === null || currentName === '') {
       alert('Je bent niet (goed) ingelogd, log alsjeblieft opnieuw in!');
