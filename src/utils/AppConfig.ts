864ea201e4e0ed3913b5ace9e0d08ce93cb5652e
juliankorevaar <korevaar.julian2000@gmail.com>
2025-11-20 22:18:37 +0100

    feat: update race to 22

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -7,7 +7,7 @@ export const AppConfig = {
   points_good_pick_race: [3, 2, 1],
   points_good_pick_bonus: 3,
   amount_of_races: 24,
-  current_race: 21,
+  current_race: 22,
   amount_of_kwali_picks: 3,
   amount_of_race_picks: 3,
 };
